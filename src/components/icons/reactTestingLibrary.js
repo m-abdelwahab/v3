deebb46038b7db6defa0063dbe583aa02c1268ab
Mahmoud Abdel Wahab <mahmoud3799@gmail.com>
2020-02-16 22:04:13 +0200

    update a11y

--- src/components/icons/reactTestingLibrary.js
+++ src/components/icons/reactTestingLibrary.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-const IconReactTestingLib = () => <div style={{ fontSize: "24px" }}><span role="img" aria-labelledby="goat">🐐</span></div>
+const IconReactTestingLib = () => <div style={{ fontSize: "24px" }}><span role="img" aria-label="goat">🐐</span></div>
 
 export default IconReactTestingLib

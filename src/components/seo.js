1f855e63e3834e5f2218fb09d49275bc6b1dd5cd
Mahmoud Abdel Wahab <mahmoud3799@gmail.com>
2020-05-23 02:40:51 +0200

    added social images

--- src/components/seo.js
+++ src/components/seo.js
@@ -48,7 +48,7 @@ const SEO = ({ description, lang, meta, title, image }) => {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
